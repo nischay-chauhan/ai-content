d04850e059c142756a81e875df8ebf082dde2e13
nischay-chauhan <masternischay@outlook.com>
2024-07-19 14:16:42 +0530

    data rendering for the different ai templates

--- app/dashboard/_components/promts.tsx
+++ app/dashboard/_components/promts.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Data from '@/app/(doc)/Data';
+import { PromptAreaProps } from '@/interface/interface';
+import React from 'react';
 
 function PromptArea() {
   return (
-    <div>PromptArea</div>
-  )
+    <div>
+      {Data.map((item: PromptAreaProps, i: number) => (
+        <div className='p-2 rounded' key={item.aiPrompt}>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <p className='text-lg font-bold text-black'>{item.name}</p>
+            <p className='text-gray-600'>{item.desc}</p>
+            <p className='text-gray-500'>{item.category}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default PromptArea
+export default PromptArea;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchSec from './_components/searrch'
+import PromptArea from './_components/promts'
 
 function Dashboard() {
   return (
-    <div>
+    <div className=''>
       <SearchSec />
+      <PromptArea />
     </div>
   )
 }
